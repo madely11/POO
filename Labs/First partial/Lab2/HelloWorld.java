f5ec1add65d53dd4412ca37fe0b5515112b34461
MadelyBetancourt <madelybetancourt@gmail.com>
2019-04-09 10:14:44 -0500

    Laboratory two

---
+++ Labs/First partial/Lab2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello world of programmers from Zambrano");
+		System.out.println("Bye world Diego Alexander Zurita Paredes");
+		System.out.println("Bye world by Zambrano Andrade Washington Andres");
+		System.out.println("Bye bye world Diego Alexander Zurita Paredes");
+    }
+}
---
+++ Labs/First partial/Lab2/HelloZurita.java
@@ -0,0 +1,5 @@
+public class HelloZurita{
+	public static void main(String[] args){
+		System.out.println("Hello from Diego Zurita");
+	}
+}
